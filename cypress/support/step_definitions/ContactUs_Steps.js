9bbd3926b199835da7e5a4682fc8aee122b4b145
Roland Halász <roland.halasz@outlook.com>
2024-02-11 22:15:55 +0100

    feat: define positive test scenario steps

---
+++ cypress/support/step_definitions/ContactUs_Steps.js
@@ -0,0 +1,26 @@
+import { When, Then } from '@badeball/cypress-cucumber-preprocessor';
+
+When(`I type a first name`, () => {
+  cy.get('[name="first_name"]').type('John');
+});
+
+When(`I type a last name`, () => {
+  cy.get('[name="last_name"]').type('Snow');
+});
+
+When(`I enter an email address`, () => {
+  cy.get('[name="email"]').type('winteriscoming@mail.com');
+});
+
+When(`I type a comment`, () => {
+  cy.get('textarea[name="message"]').type('You know nothing');
+});
+
+When(`I click on the submit button`, () => {
+  cy.get('[type="submit"]').click();
+});
+
+Then(
+  `I should be presented with a successful contact us submission message`,
+  () => {}
+);
--- cypress/support/step_definitions/Homepage_Steps.js
+++ cypress/support/step_definitions/Homepage_Steps.js
@@ -1,7 +1,11 @@
-import { Given } from '@badeball/cypress-cucumber-preprocessor';
+import { Given, When } from '@badeball/cypress-cucumber-preprocessor';
 
 const url = 'https://www.webdriveruniversity.com';
 
 Given(`I navigate to the WebdriverUniversity homepage`, () => {
   cy.visit(url);
+});
+
+When(`I click on the contact us button`, () => {
+  cy.get('#contact-us').invoke('removeAttr', 'target').click();
 });
