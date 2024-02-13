246a61cd86b83977b4998a531460031b729caee1
Roland Halász <roland.halasz@outlook.com>
2024-02-13 01:05:38 +0100

    chore: disable video recording

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,7 @@ import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esb
 export default defineConfig({
   e2e: {
     specPattern: '**/*.feature',
+    video: false,
     async setupNodeEvents(
       on: Cypress.PluginEvents,
       config: Cypress.PluginConfigOptions
